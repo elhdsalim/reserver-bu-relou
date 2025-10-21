d4d190569b31d3ab82421d2278f4ba9f559cdf1e
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-10-21 23:03:56 +0200

    Added a little test with pushover

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { verify } from "./affluences.js";
 import cron from "node-cron";
+import { Pushover } from "pushover-js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pushover = new Pushover(process.env.PO_USER!, process.env.PO_TOKEN!)
 
 const DATE = "2025-10-22";
 const HOUR = "15:00";
@@ -7,7 +12,11 @@ const DURATION = "120";
 
 (async() => {
     const job = cron.schedule("*/5 * * * * *", async () => {
-        const hey = await verify(DATE, HOUR, DURATION);
-        console.log(new Date().toISOString(), hey)
+        const availability = await verify(DATE, HOUR, DURATION);
+        if (availability) {
+            console.log(new Date().toISOString(), availability);
+            await pushover.send('Salle libérée', availability[0]?.resource_name);
+            job.stop();
+        }
     });
 })();
