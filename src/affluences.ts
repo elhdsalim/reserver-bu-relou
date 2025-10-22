1c49d918d4a7c73a0efbd19ca337b7b968661903
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-10-22 21:33:33 +0200

    Added a method to manage the pushover notification

--- src/affluences.ts
+++ src/affluences.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import type { AvailabilityResource } from './types.js';
+import type { ScheduledTask } from 'node-cron';
+import type { Pushover } from 'pushover-js';
 
 const ENDPOINT = "https://reservation.affluences.com/api/resources/52ca9bef-5796-43c0-a12a-80d9fb3fc0d3/available?"
-// add date=2025-10-21&start_hour=16:00&duration=60&type=1
 
 async function checkAvailability(date : string, start_hour : string, duration : string) : Promise<AvailabilityResource[]> {
     const URL = `${ENDPOINT}date=${date}&start_hour=${start_hour}&duration=${duration}&type=1`
@@ -21,4 +22,18 @@ export async function verify(date : string, hour : string, duration : string) : 
     }
 
     return null;
+}
+
+export async function handleAvailability(availability : AvailabilityResource[] | null, job : ScheduledTask, pushover : Pushover) : Promise<any> {
+    if (availability) {
+        console.log(new Date().toISOString(), availability);
+        const roomName = availability[0]?.resource_name
+        
+        await pushover.setSound('cashregister')
+        .setPriority(1, 60, 30)
+        .setHtml()
+        .send("Une salle s'est libérée", roomName);
+        
+        job.stop();
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { verify } from "./affluences.js";
+import { handleAvailability, verify } from "./affluences.js";
 import cron from "node-cron";
 import { Pushover } from "pushover-js";
 import dotenv from 'dotenv';
@@ -6,17 +6,13 @@ dotenv.config();
 
 const pushover = new Pushover(process.env.PO_USER!, process.env.PO_TOKEN!)
 
-const DATE = "2025-10-22";
+const DATE = "2025-10-23";
 const HOUR = "15:00";
 const DURATION = "120";
 
 (async() => {
     const job = cron.schedule("*/5 * * * * *", async () => {
         const availability = await verify(DATE, HOUR, DURATION);
-        if (availability) {
-            console.log(new Date().toISOString(), availability);
-            await pushover.send('Salle libérée', availability[0]?.resource_name);
-            job.stop();
-        }
+        await handleAvailability(availability, job, pushover);
     });
 })();
